3a5f22da9d7b0cc2a83a25de9af64fd14d5a89a7
brian.weinfeld+cuny@gmail.com <brian.weinfeld+cuny@gmail.com>
2018-03-21 16:13:57 -0400

    update sql file

--- scripts/data_scientist.sql
+++ scripts/data_scientist.sql
@@ -1,41 +1,33 @@
-DROP TABLE IF EXISTS datascience;
 DROP TABLE IF EXISTS teaching;
 DROP TABLE IF EXISTS algorithm;
 DROP TABLE IF EXISTS method;
 DROP TABLE IF EXISTS tool;
+DROP TABLE IF EXISTS datascience;
 
 CREATE TABLE datascience (
   id INTEGER PRIMARY KEY NOT NULL,
   gender VARCHAR(255) NOT NULL,
   country VARCHAR(255),
-  age VARCHAR(255),
-  employment_status VARCHAR(255),
-  current_job_title VARCHAR(255),
-  title_fit VARCHAR(255),
-  past_job_title VARCHAR(255),
-  current_employer_type VARCHAR(255),
-  formal_education VARCHAR(255),
-  major VARCHAR(255),
-  tenure VARCHAR(255),
-  first_training VARCHAR(255),
-  employer_industry VARCHAR(255),
-  employer_size VARCHAR(255),
-  employer_size_change VARCHAR(255),
-  employer_ml_time VARCHAR(255),
-  empoyer_search_method VARCHAR(255),
-  university_importance VARCHAR(255),
-  job_function VARCHAR(255),
-  remote_work VARCHAR(255),
-  compensation_amount VARCHAR(255),
-  compensation_currenty VARCHAR(255)
+  age VARCHAR(255)
   );
+
+LOAD DATA LOCAL INFILE 'C:\\Users\\Brian\\Desktop\\GradClasses\\Spring18\\607\\607project3\\tidied_csv\\data_scientist.csv' 
+INTO TABLE datascience
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS(id, gender, @country, @age)
+SET
+country = nullif(@country,'NA'),
+age = nullif(@age,'NA')
+;
 
 CREATE TABLE teaching (
   id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
   ds_id INTEGER NOT NULL,
   category VARCHAR(100) NOT NULL,
   percent INTEGER NOT NULL,
-  foreign key(ds_id) references datascience(ds_id)
+  foreign key(ds_id) references datascience(id)
   );
   
 LOAD DATA LOCAL INFILE 'C:\\Users\\Brian\\Desktop\\GradClasses\\Spring18\\607\\607project3\\tidied_csv\\learning_category.csv' 
@@ -51,7 +43,7 @@ CREATE TABLE algorithm(
     id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
     ds_id INTEGER NOT NULL,
     algorithm VARCHAR(255) NOT NULL,
-    foreign key(ds_id) references datascience(ds_id)
+    foreign key(ds_id) references datascience(id)
     );
     
 LOAD DATA LOCAL INFILE 'C:\\Users\\Brian\\Desktop\\GradClasses\\Spring18\\607\\607project3\\tidied_csv\\algorithms.csv' 
@@ -68,7 +60,7 @@ CREATE TABLE method(
     datasetsize VARCHAR(50) NOT NULL,
     method VARCHAR(255) NOT NULL,
     frequency VARCHAR(50),
-    foreign key(ds_id) references datascience(ds_id)
+    foreign key(ds_id) references datascience(id)
     );
     
 LOAD DATA LOCAL INFILE 'C:\\Users\\Brian\\Desktop\\GradClasses\\Spring18\\607\\607project3\\tidied_csv\\methods.csv' 
@@ -86,7 +78,7 @@ CREATE TABLE tool(
     ds_id INTEGER NOT NULL,
     tool_name VARCHAR(255) NOT NULL,
     frequency VARCHAR(255) NOT NULL,
-    foreign key(ds_id) references datascience(ds_id)
+    foreign key(ds_id) references datascience(id)
     );
     
 LOAD DATA LOCAL INFILE 'C:\\Users\\Brian\\Desktop\\GradClasses\\Spring18\\607\\607project3\\tidied_csv\\tool_use.csv' 
